42219a4845665031cb3d7cc8fd987d31aaca0b62
charles <charlesramos18@gmail.com>
2022-01-19 10:14:53 +0100

    código sin javadoc y7258218f

---
+++ src/charles_y7258218f/CHARLES_Y7258218F.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package charles_y7258218f;
+
+/**
+ *
+ * @author charlesramos
+ */
+public class CHARLES_Y7258218F {
+
+    static final double FACTOR_HORA_EXTRA = 1.2;
+    private String nombreCompleto, dni;
+    private double salarioBasePorHora = 10.56;
+
+    public CHARLES_Y7258218F(String nombreCompleto, String dni) {
+        this.nombreCompleto = nombreCompleto;
+        this.dni = dni;
+    }
+
+    public double getSalarioEsteMes(int horasExtra) {
+        double cantidadExtra, salarioFinal;
+        cantidadExtra = horasExtra * this.salarioBasePorHora
+                * FACTOR_HORA_EXTRA;
+        salarioFinal = this.salarioBasePorHora * 40 * 4 + cantidadExtra;
+        salarioFinal = (double) Math.round(salarioFinal * 100d) / 100d;
+        return salarioFinal;
+    }
+    // Este método no lo comentaremos con JavaDoc
+
+    public static void main(String[] args) {
+        CHARLES_Y7258218F emp = new CHARLES_Y7258218F("Charles ramos ", "y7258218f");
+        int horasExtra = 2;
+        System.out.println("El salario de " + emp.nombreCompleto + " este mes será de " + emp.getSalarioEsteMes(horasExtra) + " euros, al hacer " + horasExtra + " horas extra.");
+    }
+
+}
